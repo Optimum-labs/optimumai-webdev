424f3f9a786518200b498b62309c1cd8f1798b9b
ajay-dhangar <ajaydhangar49@gmail.com>
2024-05-01 21:10:40 +0530

    added team

--- src/data/teamMembers.js
+++ src/data/teamMembers.js
@@ -1,44 +1,76 @@
-const teamMembers = [
-  {
-    name: 'Muhammad Yahiya',
-    role: 'Founder & CEO',
-    imageUrl: 'https://avatars.githubusercontent.com/u/47945063?v=4',
-    social: {
-      twitter: 'https://twitter.com/',
-      github: 'https://github.com/muhammadyahiya',
-      linkedin: 'https://www.linkedin.com/in/shaik-muhammad-yahiya/',
-    },
-  },
-  {
-    name: 'Basanth Kalanoor',
-    role: 'CTO & Co-Founder',
-    imageUrl: 'https://github.com/basanthsk.png',
-    social: {
-      twitter: 'https://twitter.com/',
-      github: 'https://github.com/basanthsk',
-      linkedin: 'https://www.linkedin.com/in/basanthsk/',
-    },
-  },
-  {
-    name: 'Ajay Dhangar',
-    role: 'Full Stack Engineer',
-    imageUrl: 'https://github.com/ajay-dhangar.png',
-    social: {
-      twitter: 'https://twitter.com/CodesWithAjay',
-      github: 'https://github.com/Ajay-Dhangar',
-      linkedin: 'https://www.linkedin.com/in/ajay-dhangar/',
-    },
-  },
-  {
-    name: 'Anand Shende',
-    role: 'Head of Product',
-    imageUrl: 'https://avatars.githubusercontent.com/u/24247490',
-    social: {
-      twitter: 'https://twitter.com/',
-      github: 'https://github.com/anandshende',
-      linkedin: 'https://www.linkedin.com/in/anandshende/',
-    },
-  },
-];
+import TracyGreenwood from './community/tracy-greenwood.jpg';
+import KrishnaKishoreGajjala from './community/krishna-kishore.png';
+// import AnandShende from './community/anand-shende.jpg';
+// import AjayDhangar from './community/ajay-dhangar.jpg';
+// import BasanthKalanoor from './community/basanth-kalanoor.jpg';
+// import MuhammadYahiya from './community/muhammad-yahiya.jpg';
+import PrathimaKadari from './community/prathima-kadari.png';
 
-export default teamMembers;
+
+const teamMembers = [
+    {
+      name: 'Muhammad Yahiya',
+      role: 'Founder & CEO',
+      imageUrl: 'https://avatars.githubusercontent.com/u/47945063?v=4',
+      social: {
+        email: '',
+        linkedin: 'https://www.linkedin.com/in/shaik-muhammad-yahiya/',
+      },
+    },
+    {
+      name: 'Basanth Kalanoor',
+      role: 'CTO & Co-Founder',
+      imageUrl: 'https://github.com/basanthsk.png',
+      social: {
+        email: '',
+        linkedin: 'https://www.linkedin.com/in/basanthsk/',
+      },
+    },
+    {
+      name: 'Ajay Dhangar',
+      role: 'Full Stack Engineer',
+      imageUrl: 'https://github.com/ajay-dhangar.png',
+      social: {
+        email: 'mailto:ajaydhangar49@gmail.com',
+        linkedin: 'https://www.linkedin.com/in/ajay-dhangar/',
+      },
+    },
+    {
+      name: 'Anand Shende',
+      role: 'Head of Product',
+      imageUrl: 'https://avatars.githubusercontent.com/u/24247490',
+      social: {
+        email: '',
+        linkedin: 'https://www.linkedin.com/in/anandshende/',
+      },
+    },
+    {
+      name: 'Prathima Kadari',
+      role: 'Head of Community',
+      imageUrl: PrathimaKadari,
+      social: {
+        email: '',
+        linkedin: 'https://www.linkedin.com/in/prathima-kadari/',
+      },
+    },
+    {
+      name: 'Tracy Greenwood',
+      role: 'Product Manager',
+      imageUrl: TracyGreenwood,
+      social: {
+        email: '',
+        linkedin: 'https://www.linkedin.com/in/tracy-greenwood-mba-pmp/',
+      },
+    },
+    {
+      name: 'Krishna Kishore Gajjala',
+      role: 'Product Manager',
+      imageUrl: KrishnaKishoreGajjala,
+      social: {
+        email: '',
+        linkedin: 'https://www.linkedin.com/in/krishna-kishore-gajjala-b254112/',
+      },
+    },
+  ];
+  
+  export default teamMembers;
