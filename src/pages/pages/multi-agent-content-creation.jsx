1a4f3428e1ff8ce04ae247291c851c805fb181be
Yahya <47945063+muhammadyahiya@users.noreply.github.com>
2024-08-31 20:08:56 +0530

    Create multi-agent-content-creation.jsx
    Created multi-agent-content-creation.jsx

---
+++ src/pages/pages/multi-agent-content-creation.jsx
@@ -0,0 +1,5 @@
+import MultiAgentContentCreation from '../public/community/components/challenge-1'
+
+export default function MultiAgentContentCreationPage() {
+  return <MultiAgentContentCreation />
+}
